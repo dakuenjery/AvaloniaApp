a05652eebb2b76d9bda450f3a56838b453c48b95
Nikolay Golovko <dakuenjery@gmail.com>
2020-04-09 22:03:47 +0300

    Commit

---
+++ AvaloniaAndroid/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Avalonia.Android;
+using Avalonia;
+using Todo;
+
+namespace AvaloniaAndroid {
+	[Activity(Label = "@string/app_name", MainLauncher = true)]
+	public class MainActivity : AvaloniaActivity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			if (Avalonia.Application.Current == null) {
+				AppBuilder.Configure<App>()
+					.UseAndroid()
+					.SetupWithoutStarting();
+				Content = new Todo.Views.TodoListView();
+			}
+
+			base.OnCreate(savedInstanceState);
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+}
